fb4de9461fc72cabfb2ffbac26adba2a60e81c14
tyg3rr <ljensen1203@gmail.com>
2023-10-13 15:15:16 -0400

    edit return type to support sep. fence references

--- R/fences.R
+++ R/fences.R
@@ -6,17 +6,19 @@
 #'
 #' @examples
 #' fences(demo$variable4)
+#' fences(demo$variable4)$Lower
+#' fences(demo$variable4)$Upper
 #'
 
 fences <- function(x){
-  Q1 <- stats::quantile(x, 0.25)
-  Q3 <- stats::quantile(x, 0.75)
-  IQR_value <- stats::IQR(x)
-  k <- 1.5
-  lower_fence <- Q1 - k * IQR_value
-  upper_fence <- Q3 + k * IQR_value
+    Q1 <- stats::quantile(x, 0.25)
+    Q3 <- stats::quantile(x, 0.75)
+    IQR_value <- stats::IQR(x)
+    k <- 1.5
+    lower_fence <- Q1 - k * IQR_value
+    upper_fence <- Q3 + k * IQR_value
 
-  cat(" Lower Fence:", lower_fence, "\n",
-      "Upper Fence:", upper_fence)
-
+    return(
+        data.frame(Lower = lower_fence, Upper = upper_fence)
+    )
 }
